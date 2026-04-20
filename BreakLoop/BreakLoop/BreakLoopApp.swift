9e3c35cd86beb002418d204fc2a9e3482f1b7496
Master Mister <master.mister@gmail.com>
2026-04-20 11:16:49 +0200

    Project Creation

---
+++ BreakLoop/BreakLoop/BreakLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakLoopApp.swift
+//  BreakLoop
+//
+//  Created by Arjang Khademi on 20.04.26.
+//
+
+import SwiftUI
+
+@main
+struct BreakLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
